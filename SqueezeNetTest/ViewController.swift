410c8c35b468b36f109fece90ca71256c07421c6
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-12-08 10:13:24 +0100

    Initial Commit

---
+++ SqueezeNetTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SqueezeNetTest
+//
+//  Created by Søren Nielsen on 08/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
